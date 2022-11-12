fa05b972c2f0e27b6621bcc03da4ae74c893b78f
abdyusma <111057405+abdyusma@users.noreply.github.com>
2022-11-12 22:14:41 +0800

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,58 @@
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources"
+  location = "West Europe"
+}
+
+resource "azurerm_monitor_action_group" "main" {
+  name                = "example-actiongroup"
+  resource_group_name = azurerm_resource_group.example.name
+  short_name          = "p0action"
+
+  webhook_receiver {
+    name        = "callmyapi"
+    service_uri = "http://example.com/alert"
+  }
+}
+
+resource "azurerm_network_security_group" "example" {
+  name                = "acceptanceTestSecurityGroup1"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+
+  security_rule {
+    name                       = "test123"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "*"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  tags = {
+    owner = "rahman"
+  }
+}
+
+resource "azurerm_monitor_activity_log_alert" "main" {
+  name                = "example-activitylogalert"
+  resource_group_name = azurerm_resource_group.example.name
+  scopes              = [azurerm_resource_group.example.id]
+  description         = "This alert will monitor a specific storage account updates."
+
+  criteria {
+    resource_id    = azurerm_network_security_group.example.id
+    operation_name = "Microsoft.Network/networkSecurityGroups/write"
+    category       = "Recommendation"
+  }
+
+  action {
+    action_group_id = azurerm_monitor_action_group.main.id
+
+    webhook_properties = {
+      from = "terraform"
+    }
+  }
+}
